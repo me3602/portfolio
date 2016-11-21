40e143d56f091bd4987ff3adec19653ce6b6ef91
me3602 <me3602@gmail.com>
2016-11-21 16:34:07 +0900

    begin

---
+++ portfolio/src/main/java/kr/co/fun25/friday/controller/PortfolioController.java
@@ -0,0 +1,18 @@
+package kr.co.fun25.friday.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping(value="/portfolio")
+public class PortfolioController {
+
+	@RequestMapping(value="/intro", method=RequestMethod.GET)
+	public ModelAndView getIntro(ModelAndView mav){
+		
+		mav.setViewName("portfolio/intro");
+		return mav;
+	}
+}
